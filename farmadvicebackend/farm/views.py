627561cfd3e35c78e66e63e641bb30f86005de74
wakoTheDev <162966971+wakoTheDev@users.noreply.github.com>
2024-07-16 17:25:05 +0000

    configuration of django rest api

--- farmadvicebackend/farm/views.py
+++ farmadvicebackend/farm/views.py
@@ -3,3 +3,37 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request,'index.html',{})
+
+from rest_framework import viewsets
+from rest_framework.response import Response
+from .models import Todo
+from .serializers import TodoSerializer
+
+class TodoViewSet(viewsets.ModelViewSet):
+  queryset = Todo.objects.all()
+  serializer_class = TodoSerializer
+
+  def create(self, request, *args, **kwargs):
+    serializer = self.get_serializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    self.perform_create(serializer)
+    headers = self.get_success_headers(serializer.data)
+    return Response(serializer.data, status=201, headers=headers)
+
+  def update(self, request, *args, **kwargs):
+    partial = kwargs.pop('partial', False)
+    instance = self.get_object()
+    serializer = self.get_serializer(instance, data=request.data, partial=partial)
+    serializer.is_valid(raise_exception=True)
+    self.perform_update(serializer)
+    if getattr(instance, '_prefetched_objects_cache', None):
+      # If 'prefetch_related' has been applied to a queryset, we need to
+      # forcibly invalidate the prefetch cache on the instance.
+      instance._prefetched_objects_cache = {}
+    return Response(serializer.data)
+
+  def destroy(self, request, *args, **kwargs):
+    instance = self.get_object()
+    instance.delete()
+    return Response(status=204)
+
--- farmadvicebackend/farmadvicebackend/urls.py
+++ farmadvicebackend/farmadvicebackend/urls.py
@@ -17,8 +17,14 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path,include
+from rest_framework import routers
+from farm import views
+
+router = routers.DefaultRouter()
+router.register(r'todos', views.TodoViewSet)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('/',include('farm.urls')),
+    path('', include(router.urls)),
 ]
+
